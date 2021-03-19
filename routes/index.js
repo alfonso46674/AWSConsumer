e737aacf98b20f6629ba625596f12430a08f9c2e
Alponcho46674 <is713932@iteso.mx>
2021-03-18 19:49:03 -0600

    Feat: Add /autor Endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+const autorRoute = require('./autor')
+
+router.use('/autor',autorRoute)
+
 //Poner la vista de home por defecto al iniciar el servidor
 router.get('/',(req,res)=>{
     res.render('home',{
